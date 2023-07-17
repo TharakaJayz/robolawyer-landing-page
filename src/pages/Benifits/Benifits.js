d80035990468545f3382229fda8254866383143f
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-07-17 13:57:26 +0530

    finished benificts page with responsive fetures

--- src/pages/Benifits/Benifits.js
+++ src/pages/Benifits/Benifits.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import classes from './Benifits.module.css';
+import React from "react";
+import classes from "./Benifits.module.css";
+import imgage from "../../assets/benifict_img.png";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const Benifits = () => {
   return (
-    <div  className={classes.benificts}  id = 'benifits-section'>
-      benificts
-    </div>
-  )
-}
+    <div className={classes.benificts_main} id="benifits-section">
+      <div className={classes.Benifits_main_content}>
+        <div className={classes.Benifits_main_content_L}>
+          <img src={imgage} alt="business men" />
+        </div>
+        <div className={classes.Benifits_main_content_R}>
+          <section className={classes.main_content_R_sec1}>BENEFITS</section>
+          <section className={classes.main_content_R_sec2}>
+            What you will <span>get </span>
+          </section>
+          <section className={classes.main_content_R_sec3}>
+            <section>
+              {" "}
+              <BsFillCheckCircleFill className={classes.svg} />
+              <p>Easily access legal information and guidance</p>
+            </section>
+            <section>
+              {" "}
+              <BsFillCheckCircleFill className={classes.svg} />
+              <p>Reduce costs by accessing legal resources</p>
+            </section>{" "}
+            <section>
+              {" "}
+              <BsFillCheckCircleFill className={classes.svg} />
+              <p>Receive immediate responses and solutions, saving valuable time</p>
+            </section>{" "}
+            <section>
+              {" "}
+              <BsFillCheckCircleFill className={classes.svg} />
+              <p>Trust in accurate and reliable information based on up-to-date legal principles</p>
+            </section>{" "}
 
-export default Benifits
+           
+            <section>
+              {" "}
+              <BsFillCheckCircleFill className={classes.svg} />
+              <p>Receive tailored legal advice and guidance that addresses your unique needs and concerns.</p>
+            </section>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Benifits;
