0f06f7a86e375c03e34d94d3f70c52c8f2dba54c
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-07-18 14:31:08 +0530

    Pricing section completed

--- src/pages/pricing/Pricing.js
+++ src/pages/pricing/Pricing.js
@@ -3,12 +3,47 @@ import React, { useState } from "react";
 import classes from "./Pricing.module.css";
 
 import Switch from "react-switch";
+import PricingCard from "./PricingCard";
+
+import { MdDone } from "react-icons/md";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Pricing = () => {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(true);
   const handleChange = (nextChecked) => {
     setChecked(nextChecked);
   };
+
+  const detailsForPriceCard = [
+    {
+      name: "Freebie",
+      desc: "Ideal for individuals who need quick access to basic features.",
+      price: "0",
+      points: [
+        { svg: <MdDone  className={classes.svg_done}/> , desc: <span  className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <AiOutlineClose  className={classes.svg_close}/> , desc:<span className={classes.span_close}>Lorem ipsum</span>},
+        { svg: <AiOutlineClose  className={classes.svg_close}/> , desc:<span className={classes.span_close}>Lorem ipsum</span>},
+        { svg: <AiOutlineClose  className={classes.svg_close}/> , desc:<span className={classes.span_close}>Lorem ipsum</span>},
+        { svg: <AiOutlineClose  className={classes.svg_close}/> , desc:<span className={classes.span_close}>Lorem ipsum</span>},
+      ],
+    },
+    {
+      name: "Basic",
+      desc: "Ideal for individuals who who need advanced features and tools for client work.",
+      price: "10",
+      points: [
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+        { svg: <MdDone  className={classes.svg_done}/> , desc:<span className={classes.span_done}>Lorem ipsum</span>},
+     
+        { svg: <AiOutlineClose  className={classes.svg_close}/> , desc:<span className={classes.span_close}>Lorem ipsum</span>},
+      ],
+    },
+  ];
+
   return (
     <div id="pricing-section" className={classes.pricing_main}>
       <div className={classes.pricing_main_top}>
@@ -20,22 +55,25 @@ const Pricing = () => {
           aptent taciti sociosqu ad litora torquent per conubia nostr
         </p>
         <div className={classes.pricing_main_top_toggle}>
-            
-            {checked &&( <p>   Billed Monthly </p>   )}
-            {!checked &&( <p className={classes.toggled}>   Billed Monthly </p>   )}
-            <Switch
-              onChange={handleChange}
-              checked={checked}
-              className= {classes.react_switch}
-              onColor = "#4169E3"
-            />
-             
-                {checked &&( <p className={classes.toggled}> Billed Yearly </p>   )}
-                {!checked &&( <p > Billed Yearly </p>   )}
-          
+          {checked && <p> Billed Monthly </p>}
+          {!checked && <p className={classes.toggled}> Billed Monthly </p>}
+          <Switch
+            onChange={handleChange}
+            checked={checked}
+            className={classes.react_switch}
+            onColor="#4169E3"
+          />
+
+          {checked && <p className={classes.toggled}> Billed Yearly </p>}
+          {!checked && <p> Billed Yearly </p>}
         </div>
       </div>
-      <div className={classes.pricing_main_bottom}></div>
+      <div className={classes.pricing_main_bottom}>
+        <PricingCard      details = {detailsForPriceCard[0]}     className={classes.priceCard_style_freebie} />
+        <PricingCard      details = {detailsForPriceCard[1]}     className={classes.priceCard_style_basic} />
+        <PricingCard      details = {detailsForPriceCard[0]}     className={classes.priceCard_style_permium} />
+        <PricingCard      details = {detailsForPriceCard[0]}     className={classes.priceCard_style_lawyer} />
+      </div>
     </div>
   );
 };
---
+++ src/pages/pricing/PricingCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./PricingCard.module.css";
+import {MdDone} from 'react-icons/md';
+
+const PricingCard = (props) => {
+  return (
+    <div className={`${classes.pricingCard_main}  ${props.className} `}>
+      <h4> {props.details.name} </h4>
+      <p>{props.details.desc} </p>
+      <div className={classes.price_div}> <h1>${props.details.price} </h1> /Month</div>
+      <button>Start today</button>
+      <div className={classes.feture_div}>
+       
+        <section> {props.details.points[0].svg} {props.details.points[0].desc}</section>
+        <section> {props.details.points[1].svg} {props.details.points[1].desc}</section>
+        <section> {props.details.points[2].svg} {props.details.points[2].desc}</section>
+        <section> {props.details.points[3].svg} {props.details.points[3].desc}</section>
+        <section> {props.details.points[4].svg} {props.details.points[4].desc}</section>
+        <section> {props.details.points[5].svg} {props.details.points[5].desc}</section>
+       
+      </div>
+    </div>
+  );
+};
+
+export default PricingCard;
