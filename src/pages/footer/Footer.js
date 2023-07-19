af88791313d13260b783a06a473bdb4308d7786d
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-07-20 00:47:35 +0530

    finished all

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,122 +1,152 @@
-import React from 'react';
-import classes from './Footer.module.css';
-import {AiOutlineCopyright} from 'react-icons/ai';
-
+import React from "react";
+import classes from "./Footer.module.css";
+import { AiOutlineCopyright } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className={classes.footer_main}>
       <div className={classes.footer_main_content}>
-     <div className={classes.footer_main_sub1}>
-       <section className={classes.main_sub_sec1}>
-        <span>Product</span>
-        <span   >Company</span>
-       </section>
-       <section className={classes.main_sub_sec2}>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-       </section>
-     </div>
-     
-     <div className={classes.footer_main_sub1}>
-       <section className={classes.main_sub_sec1}>
-        <span>Product</span>
-        <span >Company</span>
-       </section>
-       <section className={classes.main_sub_sec2}>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-       </section>
-     </div>
-     
-     <div className={classes.footer_main_sub1}>
-       <section className={classes.main_sub_sec1}>
-        <span>Product</span>
-        <span  >Company</span>
-       </section>
-       <section className={classes.main_sub_sec2}>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a className={classes.sec}  href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-        <section  >
-          <a href = "#">Overview</a>
-          <a  className={classes.sec} href = "#">About us</a>
-        </section>
-       </section>
-     </div>
-     
-     
-    
-     </div>
-     <div className={classes.footer_end}>
-      <AiOutlineCopyright /> 
-      <a href='#robolawyer'>2023 Robolawyer. All rights reserved.  </a>
+        <div className={classes.footer_main_sub1}>
+          <section className={classes.main_sub_sec1}>
+            <span>Product</span>
+            <span>Company</span>
+          </section>
+          <section className={classes.main_sub_sec2}>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+          </section>
+        </div>
 
-     </div>
-     
+        <div className={classes.footer_main_sub1}>
+          <section className={classes.main_sub_sec1}>
+            <span>Resources</span>
+            <span>Use cases</span>
+          </section>
+          <section className={classes.main_sub_sec2}>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+          </section>
+        </div>
+
+        <div className={classes.footer_main_sub1}>
+          <section className={classes.main_sub_sec1}>
+            <span>Social</span>
+            <span>Legal</span>
+          </section>
+          <section className={classes.main_sub_sec2}>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+            <section>
+              <a href="#">Overview</a>
+              <a className={classes.sec} href="#">
+                About us
+              </a>
+            </section>
+          </section>
+        </div>
+      </div>
+      <div className={classes.footer_end}>
+        <AiOutlineCopyright />
+        <a href="#robolawyer">2023 Robolawyer. All rights reserved. </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
